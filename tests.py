b31561201a5410f0e1dff14e8437c5c2c73002c2
H4estu <zach.wallace@live.com>
2022-08-26 23:28:39 -0700

    Add tests for handling bad searches

---
+++ tests.py
@@ -0,0 +1,31 @@
+"""tests.py
+
+Testing Module
+
+At a minimum, the following should be tested:
+
+
+Searches failing to return any cards
+Both sides of dual-face cards are rendered
+"""
+
+from pricescry import CardGetter
+
+class TestSearches:
+    getter = CardGetter()
+
+    def test_ambiguous_search(self):
+        """Searches returning too many results (ambiguous search string)."""
+        ambiguous_string = 'Portal'
+        result = self.getter.search_card(ambiguous_string)
+        assert (result['object'], result['type']) == ('error', 'ambiguous')
+
+    def test_card_not_found(self):
+        unknown_card = 'this_card_does_not_3X$1$T'
+        result = self.getter.search_card(unknown_card)
+        assert (result['object'], result['status']) == ('error', 404)
+
+
+# Both sides of dual-face cards are rendered
+
+
